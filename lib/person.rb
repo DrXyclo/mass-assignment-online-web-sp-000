716c03e738e6f654260580d9fe0e6ecb7dcf52ec
Dr. XyClo <sdcarmody@gmail.com>
2019-11-17 15:32:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
+  def initialize(some_hash)
 
-def initialize(some_hash)
   
-  
-end 
+  end 
 
 end
